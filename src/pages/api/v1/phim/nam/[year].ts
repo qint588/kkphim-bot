66eb4b322bb3b5d077a8b59ed718dbecb96c3e3d
Quốc Trần <tranaiquoc4499@gmail.com>
2024-11-05 08:07:34 +0700

    Build: api year

---
+++ src/pages/api/v1/phim/nam/[year].ts
@@ -0,0 +1,91 @@
+import { IDataResponse, IErrorResponse } from "@/lib/global";
+import connectToDatabase from "@/lib/mongoose";
+import { IEpisode } from "@/models/episode.model";
+import Movie, { IMovie } from "@/models/movie.model";
+import movieService from "@/services/movie.service";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { z, ZodError } from "zod";
+
+/**
+ * @swagger
+ * /api/v1/phim/nam/{year}:
+ *   get:
+ *     description: Trả ra danh sách phim theo năm
+ *     parameters:
+ *       - in: path
+ *         name: year
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Năm
+ *         example: 1969
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *           default: 1
+ *         description: Số trang cần lấy (mặc định là 1)
+ *       - in: query
+ *         name: per_page
+ *         schema:
+ *           type: integer
+ *           default: 10
+ *         description: Số lượng phim trên mỗi trang (mặc định là 10)
+ *     responses:
+ *       200:
+ *         description: Danh sách phim theo năm
+ */
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<IDataResponse<IMovie> | IErrorResponse>
+) {
+  await connectToDatabase();
+
+  const schema = z.object({
+    page: z.number().min(1, "Page must be at least 1").optional(),
+    per_page: z
+      .number()
+      .min(1, "Per page must be at least 1")
+      .max(100, "Per page must not exceed 100")
+      .optional(),
+    year: z
+      .string()
+      .min(1)
+      .refine(
+        async (year) => {
+          const movie = await Movie.findOne({ year }).select("_id name");
+          return !!movie;
+        },
+        {
+          message: "Year does not exist in movies collection",
+        }
+      ),
+  });
+
+  try {
+    const validated = await schema.parseAsync({
+      ...req.query,
+      page: req.query?.page
+        ? parseInt(req.query?.page as string, 10)
+        : undefined,
+      per_page: req.query?.per_page
+        ? parseInt(req.query?.per_page as string, 10)
+        : undefined,
+    });
+    const response = await movieService.search(validated);
+    res.status(200).json(response);
+  } catch (error) {
+    if (error instanceof ZodError) {
+      const formattedErrors = error.errors.map((err) => ({
+        field: err.path.join("."),
+        message: err.message,
+      }));
+      return res.status(422).json({
+        message: "Invalid request",
+        errors: formattedErrors,
+      });
+    } else {
+      res.status(500).json({ message: "Server error" });
+    }
+  }
+}
